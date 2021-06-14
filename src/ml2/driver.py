70f2f7428b11f64984515c7300649199d29e3ee0
Michael Sherman <shermanm@uchicago.edu>
2021-06-14 02:17:26 +0000

    create namespace and wg link from network name

--- src/ml2/driver.py
+++ src/ml2/driver.py
@@ -26,15 +26,43 @@ class WireguardMechanismDriver(MechanismDriver):
         LOG.debug("Initializing Wireguard ML2 Driver. New!")
         super().initialize()
 
+    def _validate_channel(self, port):
+        """Ensure needed info is present.
+
+        For spoke ports, must include:
+        1. Public key for client endpoint
+        2. (optional) IP:port for client endpoint
+        """
+        pass
+
+    def _create_wg_iface(self, port: dict):
+        """Create WireGuard interface in correct namespace
+
+        1. Get tenant namespace (ensure q-l3 is enabled!)
+        2. create ifname from namespace name
+        3. ensure interface doesn't already exist
+        4. Create the stuff
+        """
+
+        network_id = port.get("network_id")
+
+        # Create network namespace
+        netns_name = f"qrouter-{network_id}"
+        subprocess.run(["sudo", "ip", "netns", "add", netns_name])
+
+        wg_if_name = f"tun-{network_id[0:8]}"
+        # Create wireguard interface
+        subprocess.run(
+            ["sudo", "ip", "link", "add", "dev", wg_if_name, "type", "wireguard"]
+        )
+
     def create_port_precommit(self, context: PortContext):
         port = context.current
         LOG.info(f"Entered port precommit with port_info {json.dumps(port)}")
 
-        if_name = port.get("name")
+        self._validate_channel(port)
 
-        subprocess.run(
-            ["sudo", "ip", "link", "add", "dev", if_name, "type", "wireguard"]
-        )
+        self._create_wg_iface(port)
 
     def update_port_precommit(self, context: PortContext):
         LOG.debug("Entered Update Port")
