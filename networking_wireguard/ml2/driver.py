6170a0ddf4ba7e6cea33455a97fb5fdba4835384
Michael Sherman <shermanm@uchicago.edu>
2021-07-05 21:10:13 +0000

    add better comments

--- networking_wireguard/ml2/driver.py
+++ networking_wireguard/ml2/driver.py
@@ -13,11 +13,20 @@ class WireguardMechanismDriver(MechanismDriver):
     """Management of wireguard interfaces corresponding to neutron ports."""
 
     def initialize(self):
-        """Run when plugin loads."""
+        """Run when plugin loads.
+
+        This method checks all existing ports, and makes sure that any
+        wireguard hub ports have an associated wireguard interface.
+        It loads the relevant config from the config dir, and will leave the
+        port in an error state if that is not found.
+        """
         super().initialize()
 
     def create_port_precommit(self, context: PortContext):
-        """Run inside the db transaction when creating port."""
+        """Run inside the db transaction when creating port.
+
+        This creates a wireguard interface to go along with the neutron port.
+        """
         port = context.current
         vif_details = utils.get_vif_details(port)
         if WG_TYPE_KEY in vif_details:
@@ -25,7 +34,10 @@ class WireguardMechanismDriver(MechanismDriver):
             wg_port.create(port)
 
     def update_port_precommit(self, context: PortContext):
-        """Run inside the db transaction when updating port."""
+        """Run inside the db transaction when updating port.
+
+        This updates an existing wireguard interface, associated with the port.
+        """
         port = context.current
         vif_details = utils.get_vif_details(port)
         if WG_TYPE_KEY in vif_details:
@@ -35,7 +47,10 @@ class WireguardMechanismDriver(MechanismDriver):
             # wg_port.create(network_id)
 
     def delete_port_precommit(self, context: PortContext):
-        """Run inside the db transaction when deleting port."""
+        """Run inside the db transaction when deleting port.
+
+        This deletes an existing wireguard interface, associated with the port.
+        """
         port = context.current
         vif_details = utils.get_vif_details(port)
         if WG_TYPE_KEY in vif_details:
