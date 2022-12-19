097ff441756eb852409567fd5f07e4ed9dfa7ed9
Michael Sherman <shermanm@uchicago.edu>
2022-12-19 15:42:09 -0600

    ensure we set the vnic type

--- networking_wireguard/ml2/mech_driver/driver.py
+++ networking_wireguard/ml2/mech_driver/driver.py
@@ -41,7 +41,8 @@ class WireguardMechanismDriver(mech_agent.AgentMechanismDriverBase):
 
     def __init__(self):
         super(WireguardMechanismDriver, self).__init__(
-            agent_type=wg_const.AGENT_TYPE_WG
+            agent_type=wg_const.AGENT_TYPE_WG,
+            supported_vnic_types=portbindings.VNIC_NORMAL,
         )
         self.notifier = rpc.AgentNotifierApi(topics.AGENT)
         self.rpc_callbacks = WireguardRpcCallback()
